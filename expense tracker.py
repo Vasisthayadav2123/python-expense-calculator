42a544370378a6129f7046b0f59d67cc8c734580
Vasisthayadav2123 <vasisthayadav58@gmail.com>
2024-12-07 19:40:44 +0530

    added total and amount by category

--- expense tracker.py
+++ expense tracker.py
@@ -1,23 +1,24 @@
-import expense from expense
+class Expense:
+    def __init__(self, name, category, amount):
+        self.name = name
+        self.category = category
+        self.amount = float(amount)
 
 def main():
-    print(f"🎯running expense tracker")
+    print(f"🎯 running expense tracker")
+    expense_file_name = "expense.csv"
     
-    get_user_expense()
-
-    save_expense_to_file()
-
-    summarize_expense()
-
+    expense = get_user_expense()
+    save_expense_to_file(expense, expense_file_name)
     
-# taking input from the user
+    summarize_expense(expense_file_name)
+
 def get_user_expense():
-
-    print(f"🎯getting user expense")
+    print(f"🎯 getting user expense")
     
-    expense_name = input(f"enter expense name:" )
-    expense_amount = float(input("enter expense amount: "))
-    print(f"{expense_amount},{expense_name}")
+    expense_name = input(f"Enter expense name: ")
+    expense_amount = float(input("Enter expense amount: "))
+    print(f"{expense_amount}, {expense_name}")
     
     expense_categories = [
         "🍎 food",
@@ -29,38 +30,55 @@ def get_user_expense():
     category_len = len(expense_categories)
     print(category_len)
     while True:
-
-        print("select a category: ")
-
+        print("Select a category: ")
         for i, category_name in enumerate(expense_categories):
-            print(f"    {i+1}. {category_name}")  
+            print(f"{i+1}. {category_name}")  
 
-
-        try:    
-
-            selected_category = int(input(f"enter the category [1 - {category_len}] : ")) - 1 
+        try:
+            selected_index = int(input(f"Enter the category [1 - {category_len}] : ")) - 1 
         except Exception:
+            print("You entered a string/char instead of a number") 
 
-            print("you entered an string/char instead of number") 
-
-
-        if selected_category in range (category_len):
-
-            print(f" you selected: {expense_categories[selected_category]}")
-
-
-            break
+        if selected_index in range(category_len):
+            print(f"You selected: {expense_categories[selected_index]}")
+            selected_category = expense_categories[selected_index]
+            new_expense = Expense(name=expense_name, category=selected_category, amount=expense_amount)
+            return new_expense
         else:
-            print("you entered and invalid catgory")
+            print("You entered an invalid category")
 
-#saving the expenses to a file
-def save_expense_to_file():
-    print(f"🎯saving expense to file")
+def save_expense_to_file(expense: Expense, expense_file_name):
+    print(f"🎯 saving expense to file: {expense} to {expense_file_name}")
+    with open(expense_file_name, "a", encoding="utf-8") as f:
+        f.write(f"{expense.name},{expense.category},{expense.amount}\n")
 
-#summarising the expenses 
-def summarize_expense():
-    print(f"🎯summarising the expenses")
+def summarize_expense(expense_file_name):
+    print(f"🎯 summarizing the expenses in {expense_file_name}")
+    expenses = []
+    with open(expense_file_name, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        for line in lines:
+            expense_name, expense_category, expense_amount = line.strip().split(",")
+            print(expense_name, expense_category, expense_amount)
+            line_expense = Expense(
+                name=expense_name, category=expense_category, amount=expense_amount
+            )
+            expenses.append(line_expense)
 
+    amount_by_category = {}
+    for expense in expenses:
+        key = expense.category
+        if key in amount_by_category:
+            amount_by_category[key] += expense.amount
+        else:
+            amount_by_category[key] = expense.amount
+
+    print("Expenses By Category 📈:")
+    for key, amount in amount_by_category.items():
+        print(f"  {key}: ${amount:.2f}")
+
+    total_spent = sum([x.amount for x in expenses])
+    print(f"💵 Total Spent: ${total_spent:.2f}")
 
 if __name__ == "__main__": 
     main()
--- expense.py
+++
@@ -1,8 +0,0 @@
-class expense:
-
-    def __init__(self , name , category , amount ) -> None:
-        self.name = name 
-        self.category = category
-        self.amount = amount
-        
-        
